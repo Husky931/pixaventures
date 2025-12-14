59007e805f08d51da9d97f6e5c4c7ecb9a175773
Pecev Gligor <gilgor.pecev@breitling.com>
2025-12-14 13:11:41 +0800

    update

--- app/lib/client.ts
+++
@@ -1,8 +0,0 @@
-import { createStorefrontApiClient } from '@shopify/storefront-api-client';
-
-export const CLIENT = createStorefrontApiClient({
-  storeDomain:
-    process.env.SHOPIFY_STORE_DOMAIN ?? 'https://qtr1jt-wr.myshopify.com',
-  apiVersion: '2025-01',
-  publicAccessToken: process.env.STOREFRONT_PUBLIC_ACCESS_TOKEN,
-});
